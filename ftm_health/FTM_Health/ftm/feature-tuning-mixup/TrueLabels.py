79796817d2d3fb4a9960c48dab931630d08523a1
iqraaa453 <148413756+iqraaa453@users.noreply.github.com>
2026-08-15 01:10:35 +0500

    Delete ftm_health/FTM_Health/ftm/feature-tuning-mixup directory

--- ftm_health/FTM_Health/ftm/feature-tuning-mixup/TrueLabels.py
+++
@@ -1,8 +0,0 @@
-import torchxrayvision as xrv
-
-# Load your model
-model = xrv.models.DenseNet(weights="densenet121-res224-all")
-
-# Print the index and the disease name
-for i, disease in enumerate(model.pathologies):
-    print(f"Index {i}: {disease}")
